7fd3b79034943b8d56ef4788384975ca51f1a02d
Mykyta <mikita_korshak@ukr.net>
2025-01-14 11:37:15 +0200

    Add save and load function
    'save' saves tasks in file
    'load' load tasks from file

--- src/main.c
+++ src/main.c
@@ -1,23 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
 #include <string.h>
 #include <unistd.h>
 #include <pwd.h>
+#include <sys/stat.h>
 
 #define TASKS_MAX 	UCHAR_MAX
 #define DESC_MAX	USHRT_MAX
 
-struct {
-	const char name[TASKS_MAX][NAME_MAX]; 
-	const char desc[TASKS_MAX][DESC_MAX];
+struct task{
+	char name[TASKS_MAX][NAME_MAX]; 
+	char desc[TASKS_MAX][DESC_MAX];
 } tasks;
 
 static char path[PATH_MAX];
 
+void save(void);
+void load(void);
+
 int main(void)
 {
+	struct stat state;
+
 	strcpy(path, getpwuid(getuid())->pw_dir);
 	strcat(path, "/.local/share/todoas");
+
+	if (stat(path, &state))
+	{
+		if (mkdir(path, 0777))
+		{
+			fprintf(stderr, "can't create directory '%s'\n", path);
+			exit(-1);
+		}
+	}
+
+	strcat(path, "/data.bin");
 	printf("path: %s\n", path);
+
+	strcpy(tasks.name[0], "hello");
+	strcpy(tasks.desc[0], "say hello to everyone");
+	save();
+
+	load();
+	printf("name: %s\n", tasks.name[0]);
+	printf("desc: %s\n", tasks.desc[0]);
 	return 0;
+}
+
+void save(void)
+{
+	FILE *fp;
+	if (!(fp = fopen(path, "wb"))) {
+		fprintf(stderr, "error: can't save tasks\n");
+		exit(-1);
+	}
+
+	fwrite(tasks.name, sizeof(char[NAME_MAX]), TASKS_MAX, fp);
+	fwrite(tasks.desc, sizeof(char[NAME_MAX]), TASKS_MAX, fp);
+	fclose(fp);
+}
+
+void load(void)
+{
+	FILE *fp;
+	if (!(fp = fopen(path, "rb"))) {
+		fprintf(stderr, "error: can't load tasks\n");
+		exit(-1);
+	}
+
+	fread(tasks.name, sizeof(char[NAME_MAX]), TASKS_MAX, fp);
+	fread(tasks.desc, sizeof(char[DESC_MAX]), TASKS_MAX, fp);
+	fclose(fp);
 }
