bc4071f9d0e2426d01aad972307dde081a9a52d9
Mykyta <mikita_korshak@ukr.net>
2025-01-14 10:10:44 +0200

    Add base path and create struct of arrays

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <unistd.h>
+#include <pwd.h>
+
+#define TASKS_MAX 	UCHAR_MAX
+#define DESC_MAX	USHRT_MAX
+
+struct {
+	const char name[TASKS_MAX][NAME_MAX]; 
+	const char desc[TASKS_MAX][DESC_MAX];
+} tasks;
+
+static char path[PATH_MAX];
 
 int main(void)
 {
-	printf("hello\n");
+	strcpy(path, getpwuid(getuid())->pw_dir);
+	strcat(path, "/.local/share/todoas");
+	printf("path: %s\n", path);
 	return 0;
 }
